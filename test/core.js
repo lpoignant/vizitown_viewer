adca08ee7a1f456d8dbd040d95f2961b31c8b92a
Nicolas MEUNIER <n.c.meunier@gmail.com>
2014-02-04 18:40:26 +0100

    Mocking the websocket server

--- test/core.js
+++ test/core.js
@@ -1,5 +1,4 @@
 require('../src/core/Function.js');
-VWebSocket = require('../src/core/VWebSocket.js');
 var assert = require("assert");
 
 describe('Function', function () {
@@ -65,21 +64,3 @@ describe('Function', function () {
 	});
 	
 });
-/*
-describe('VWebSocket', function () {
-	
-	describe('#onopen', function () {
-		it('should be opened', function () {
-			var ws = new VWebSocket({
-				host: "127.0.0.1",
-				port: "8888",
-				uri: "/echows",
-				onopen: function () {
-					assert(true, 'socket is opened'); 
-				},
-			});
-		});
-	});
-	
-});
-*/
--- test/websocket.js
+++ test/websocket.js
@@ -1,17 +1,39 @@
 //VWebSocket = require('../src/core/VWebSocket.js');
 
 describe('VWebSocket', function () {
+	var vhost = "127.0.0.1";
+	var vport = "8888";
+	var vpath = "/ws";
 	
 	describe('#onopen', function () {
-		it('should be opened', function () {
+		it('should be opened', function (done) {
 			var ws = new VWebSocket({
-				host: "127.0.0.1",
-				port: "8888",
-				uri: "/echows",
+				host: vhost,
+				port: vport,
+				path: vpath,
 				onopen: function () {
-					assert(true, 'socket is opened'); 
-				},
+					assert(true, 'socket is opened');
+					done();
+					this.close();
+				}
 			});
+		});
+	});
+	
+	describe('#onmessage', function () {
+		it('should echo', function (done) {
+			var echo = 'echo';
+			var ws = new VWebSocket({
+				host: vhost,
+				port: vport,
+				path: vpath,
+				onmessage: function (message) {
+					assert.equal(echo, message.data);
+					done();
+				}
+			});
+			done();
+			ws.socket.send(echo);
 		});
 	});
 	
