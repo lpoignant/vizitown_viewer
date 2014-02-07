8cae4a96ed05e2cb4effca6194796197d203f42b
Nicolas MEUNIER <n.c.meunier@gmail.com>
2014-02-07 11:33:21 +0100

    Method to create control

--- src/control/ControlSwitcher.js
+++ src/control/ControlSwitcher.js
@@ -1,46 +1,59 @@
-var Controls = function(camera) {
+var ControlSwitcher = function(camera, domElement) {
     this._camera = camera;
 	this._control = true;
+	this._domElement = domElement || document;
 	
 	this._trackRotateSpeed = 1.0;
 	this._trackZoomSpeed = 1.0;
 	this._trackPanSpeed = 1.0;
 	this._trackStaticMoving = true;
 	
-	this.current = new THREE.TrackballControls(this._camera);
-	this.current.rotateSpeed = this._trackRotateSpeed;
-	this.current.zoomSpeed = this._trackZoomSpeed;
-	this.current.panSpeed = this._trackPanSpeed;
-	this.current.staticMoving = this._trackStaticMoving;
-	this.current.reset();
-}
+	this._createTrackball = function () {
+		var control = new THREE.TrackballControls(this._camera);
+		control.rotateSpeed = this._trackRotateSpeed;
+		control.zoomSpeed = this._trackZoomSpeed;
+		control.panSpeed = this._trackPanSpeed;
+		control.staticMoving = this._trackStaticMoving;
+		control.reset();
+		return control;
+	};
+	
+	this._createFlyControl = function (pointToLook) {
+		var control = new THREE.FlyControls(this._camera);
+		control.movementSpeed = 50;
+		control.rollSpeed = 0.125;
+		control.lookVertical = true;
+		//this._camera.lookAt(pointToLook);
+		console.log(control);
+		return control;
+	};
 
-Controls.prototype.changeControlMode = function() {
+	this.current = this._createTrackball();
+	
+	this._addListeners();
+};
+
+ControlSwitcher.prototype.changeControlMode = function() {
+	var pLocal = new THREE.Vector3( 0, 0, -1 );
+	console.log(this._camera);
+	var pWorld = pLocal.applyMatrix4( this._camera.matrixWorld );
+	//console.log(pWorld);
 	if (this._control) {
-		this.current = new THREE.FlyControls(this._camera);
-		this.current.movementSpeed = 50;
-		this.current.rollSpeed = 0.125;
-		this.current.lookVertical = true;
+		this.current = this._createFlyControl(pWorld);
 	}
 	else {
-		this.current = new THREE.TrackballControls(this._camera);
-		this.current.rotateSpeed = this._trackRotateSpeed;
-		this.current.zoomSpeed = this._trackZoomSpeed;
-		this.current.panSpeed = this._trackPanSpeed;
-		this.current.staticMoving = this._trackStaticMoving;
-		this.current.reset();
+		this.current = this._createTrackball();
 	}
 	this._control = !this._control;
-}
+};
 
-Controls.prototype.allowMultipleControls = function(document) {
+ControlSwitcher.prototype._addListeners = function() {
     var self = this;
-	document.addEventListener('keyup', function(e) {
-        
+	this._domElement.addEventListener('keyup', function(e) {
         var ascii_value = e.keyCode;
-        if(String.fromCharCode(ascii_value) == 'H' ) {
+		console.log(ascii_value);
+        if(String.fromCharCode(ascii_value) == 'K' ) {
             self.changeControlMode();
         }
-        
     }, false);
-}
+};
