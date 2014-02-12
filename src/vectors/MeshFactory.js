dd3b1fa02d4fabfa6ede43cd5bf775341a227f1b
Alexandre KURTH <alexandre.kurth@gmail.com>
2014-02-12 17:23:06 +0100

    todebug

--- src/vectors/MeshFactory.js
+++ src/vectors/MeshFactory.js
@@ -1,50 +1,50 @@
 //"use strict";
 
-
 /**
- * Class which ...
+ * Class which permit to convert a JSON representation of a geometry to a Three.js mesh
  * 
- * @class rMeshFactory
+ * @class MeshFactory
  * @constructor
  */
 var MeshFactory = function(args) {
-    if (typeof args === undefined) {
-        args = '{}';
-    }
-    this._pointMaterial = args.pointMaterial || new THREE.MeshBasicMaterial({color: 0xffff00}) ; // default : yellow
-    this._lineMaterial = args.lineMaterial || new THREE.LineBasicMaterial({color: 0x00ee00}) ; // default : green
-    this._polyhMaterial = args.polyhMaterial ||  new THREE.MeshLambertMaterial({color:  0xcc0000}); // default : red
+    
+    if (args === undefined) args = {};
+
+    this._pointMaterial = /*args.pointMaterial ||*/ new THREE.MeshBasicMaterial({color: 0xffff00, wireframe: true}); // default : yellow
+    this._lineMaterial = /*args.lineMaterial ||*/ new THREE.LineBasicMaterial({color: 0x00ee00, wireframe: true}); // default : green
+    this._polyhMaterial = /*args.polyhMaterial ||*/  new THREE.MeshLambertMaterial({color:  0xcc0000, wireframe: true}); // default : red
 };
 
 /**
-@method applyRaster
+@method jsonToMesh 
 */
 MeshFactory.prototype.jsonToMesh = function(jsonString) {
 
     var loader = new THREE.JSONLoader(); 
-    var geom = loader.parse(json3);
+    var geom = loader.parse(jsonString);
     var material;
     var verticesCount = geom.geometry.vertices.length;
     var facesCount = geom.geometry.faces.length;
    
     if (verticesCount == 1) {
-        console.log("point");
+
+        //console.log("point");
         material = this._pointMaterial;
+        
     }
     else if(verticesCount > 1 && facesCount == 0 ) {
-        console.log("line");
+
+        //console.log("line");
+        //geom = new THREE.Line(geom);
         material = this._lineMaterial;
+        return new THREE.Line(geom, material)
     }
     else if(verticesCount > 3 && facesCount > 0) {
-        console.log("3D")
+
+        //console.log("3D");
         material = this._polyhMaterial;
     }
-    console.log(geom);
 
-    return new THREE.Mesh(geom, material);
+    return new THREE.Mesh(geom.geometry, material);
+
 };
-
-
-
-
-
