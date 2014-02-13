ba70d1ad3044c0adba6c5e071277c94239bfe192
Nicolas MEUNIER <n.c.meunier@gmail.com>
2014-02-13 09:17:41 +0100

    adding a debugging scene

--- src/extras/DebugScene.js
+++ src/extras/DebugScene.js
@@ -1,3 +1,6 @@
+/* global FPSControl:true */
+"use strict";
+
 /**
  * Create a scene with basic elements to debug.
  * Axis helper, X-Y plane, Camera
@@ -8,45 +11,59 @@
  * @param {Document} Browser document element 
  * @param {float} Size of the debug scene
  */
-var DebugScene = function (window, document, size) {
-    //super constructor
-    THREE.Scene.call(this);
+var DebugScene = function (window, document, domElementId, size) {
+    this._scene = new THREE.Scene();
     
-    this._window = windows;
+    this._window = window;
     
     var ratio = this._window.innerWidth / this._window.innerHeight;
-    this._camera = new THREE.PerspectiveCamera(45, ratio, 0.1, size);
+    
+    this._camera = new THREE.PerspectiveCamera(30, ratio, 0.1, 1000);
+    this._camera.position.x = 10;
+    this._camera.position.y = 10;
+    this._camera.position.z = 10;
+    this._camera.lookAt(new THREE.Vector3(0, 0, 0));
     
     this._renderer = new THREE.WebGLRenderer();
     this._renderer.setSize(window.innerWidth, window.innerHeight);
     
     this._document = document;
-    this._document.getElementById(domElementId).appendChild(this._renderer.domElementId);
+    this._document.getElementById(domElementId).appendChild(this._renderer.domElement);
     
     this._axes = new THREE.AxisHelper(size);
-    this._grid = new THREE.GridHelper(size);
-    this._frustrum = new THREE.CameraHelper(this._camera);
     
-    /*this._wireMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
-    var plane = new THREE.PlaneGeometry(size, size);
-    this._grid = new THREE.Mesh(plane, this._wireMaterial);*/
-};
-DebugScene.inheritsFrom(THREE.Scene);
+    this._control = new FPSControl(this._camera, this._document);
+    this._control.listen();
 
+    this._clock = new THREE.Clock();
+    var planeGeometry = new THREE.PlaneGeometry(size, size, size/10000, size/10000);
+    var planeMaterial = new THREE.MeshBasicMaterial({color: 0xcccccc, wireframe:true});
+    this._scene.add(new THREE.Mesh(planeGeometry, planeMaterial));
+    
+    this._scene.add(this._axes);
+    this._scene.add(this._grid);
+    this._scene.add(this._camera);
+    
+    var self = this;
+    this._onWindowResize = function onWindowResize() {
+		self._camera.aspect = self._window.innerWidth / self._window.innerHeight;
+		self._camera.updateProjectionMatrix();
+		self._renderer.setSize( self._window.innerWidth, self._window.innerHeight );
+	};
+	
+	this._window.addEventListener('resize', this._onWindowResize, false );
+};
 
 DebugScene.prototype.render = function () {
-    this._window.requestAnimationFrame(render);
-    this._renderer.render(this, this._camera);
+    this._window.requestAnimationFrame(this.render.bind(this));
+    this._renderer.render(this._scene, this._camera);
+    
+    var delta = this._clock.getDelta();
+    this._control.update(delta);
 };
 
-DebugScene.prototype.lookAt = function (pos) {
-    this._camera.lookAt(pos);
-    this._camera.pos = pos;
-    this._camera.pos.z = pos.z + 50;
-};
-
-DebugScene.prototype.initialize = function () {
-    this.add(this._axes);
-    this.add(this._grid);
-    this.add(this.frustrum);
+DebugScene.prototype.lookAt = function () {
+    //this._camera.lookAt(pos);
+    //this._camera.pos = pos;
+    //this._camera.pos.z = pos.z + 50;
 };
